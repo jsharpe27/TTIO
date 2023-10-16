536aaa029130a317f90d56cdfaf3694a1b4ca6f2
Jordan Sharpe <86000863+jsharpe27@users.noreply.github.com>
2023-10-16 14:59:19 -0400

    trying to fetch posts from posts table

---
+++ ttio/app/api/get-posts/route.ts
@@ -0,0 +1,33 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request: Request){
+    const supabase = createRouteHandlerClient({ cookies });
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+    
+    if (user){
+        const { data, error } = await supabase
+            .from("posts")
+            .select("*")
+    
+    let responseJSON = {};
+        if (!error) {
+            responseJSON = { message: "Posts retrieved successfully", data };
+
+            return NextResponse.json(responseJSON);
+        } else {
+            responseJSON = { error: error };
+
+            return NextResponse.json(responseJSON)
+        }
+    }
+}
+
+
+
